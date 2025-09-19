69416acb62c177a33deea4f69a84bbff7af1619d
Rubén Marhuenda <ruben.marhuenda@iesdoctorbalmis.com>
2025-09-19 19:49:05 +0200

    Fundamentos de JS I terminado

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valor) {
     // TODO
+    if(valor > 0){
+        presupuesto = valor;
+        return valor;
+    }
+    else{
+        console.log("Valor introducido erroneo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    this.descripcion = descripcion,
+    this.valor = (valor > 0) ? valor : 0,
+    this.mostrarGasto = function(){
+        return(`Gasto correspondiente a ${descripcion} con valor ${valor} €`);
+    },
+    this.actualizarDescripcion = function(descripcion){
+        this.descripcion = descripcion;
+    },
+    this.actualizarValor = function (valor) {
+        this.valor = (valor > 0) ? valor : this.valor;
+    }
 }
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
