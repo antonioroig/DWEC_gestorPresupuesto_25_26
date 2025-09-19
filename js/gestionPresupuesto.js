939a8c7b8f70cd38000a8e0eb8fc42bebc7aa1b2
Juan León <depechejuan@gmail.com>
2025-09-19 19:49:44 +0200

    Update 19-09

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,39 +4,98 @@
 let presupuesto = 0;
 
 function actualizarPresupuesto(value) {
-    if (value < 0) {
+    const num = Number(value);
+    if (isNaN(num) || num < '') {
         console.log(`Error en el valor del presupuesto`);
         return -1;
     }
+    presupuesto = value;
     return value;
 }
 
 function mostrarPresupuesto() {
-    console.log(`Tu presupuesto actual es de: ${presupuesto}€`)
+    const text = `Tu presupuesto actual es de ${presupuesto} €`;
+    return text;
 }
 
-class Gasto {
-    constructor(descripcion, valor) {
-        this.descripcion = descripcion
-        this.valor = valor
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    this.valor = this.actualizarValor(valor);
+
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
     }
-    mostrarGasto() {
-        const desc = `Gasto correspondiente "${this.descripcion}" con Valor: ${this.valor} €`
-        return desc;
-    }
-    actualizarDescripcion(value) {
+
+    this.actualizarDescripcion = function(value) {
         this.descripcion = value;
     }
-    actualizarValor(value) {
-        if (value < 0)
-            return;
-        this.valor = value;
+    
+    this.actualizarValor = function(value) {
+        const num = Number(value);
+        if (isNaN(num) || num < 0) {
+            return 0;
+        }
+        return num;
     }
+
+    return this;
 }
 
-function CrearGasto() {
+// CrearGasto.prototype.mostrarGasto = function() {
+//     return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+// };
 
-}
+// CrearGasto.prototype.actualizarDescripcion = function(value) {
+//     this.descripcion = value;
+// };
+
+// CrearGasto.prototype.actualizarValor = function(value) {
+//     const num = Number(value); //dupe
+//     if (isNaN(num) || num < '') {
+//         return;
+//     }
+//     if (num < 0) {
+//         return;
+//     }
+
+//     this.valor = num;
+// };
+
+
+
+// function CrearGasto(descripcion, valor) {
+//     return new Gasto(descripcion, valor);
+// }
+
+// class Gasto {
+//     constructor(descripcion, valor) {
+//         this.actualizarDescripcion(descripcion);
+//         this.actualizarValor(valor);
+//     }
+    
+//     mostrarGasto() {
+//         const desc = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
+//         return desc;
+//     }
+
+//     actualizarDescripcion(value) {
+//         this.descripcion = value;
+//     }
+
+//     actualizarValor(value) {
+//         this.valor = 0;
+//         const num = Number(value); //dupe
+//         if (isNaN(num) || num < '') {
+//             return;
+//         }
+//         if (num < 0) {
+//             return;
+//         }
+
+//         this.valor = num;
+//     }
+// }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
