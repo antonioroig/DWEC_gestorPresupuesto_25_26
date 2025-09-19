483e01b009a1c81fa65f48b8c2b71736803ff61c
Tonizhan325 <angelzhan666@gmail.com>
2025-09-19 19:37:01 +0200

    Tarea gestionPresupuesto.js completado y comprobado en consola

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,32 +4,51 @@
 
 let presupuesto = 0;
 
-
 function actualizarPresupuesto(presupuestoActualizado) {
     if (presupuesto < 0)
+    {
+        console.log("El valor introducido es negativo");
         return -1;
+    }
     presupuesto = presupuestoActualizado;
+    return presupuesto
 }
 
 function mostrarPresupuesto() {
-    return "Tu presupuesto actual es de " + presupuesto + " €"
+    return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
 function CrearGasto() {
     let gasto = {
         descripcion: "el gasto le pertenece a anónimo 321",
         valor: 324,
-        mostrarGasto: function() {
+        mostrarGasto: function(descripcion, valor) {
             console.log("Gasto correspondiente a " + descripcion + " con valor " + valor + " €");        
         },
-        actualizarDescripcion: function(descripcionNueva) {
+        actualizarDescripcion: function(descripcion, descripcionNueva) {
             descripcion = descripcionNueva;
+            return descripcion;
         },
-        actualizarValor: function(valorNuevo){
+        actualizarValor: function(valor, valorNuevo){
+            if (valorNuevo < 0)
+                return;
             valor = valorNuevo;
+            return valor;
         }
     }
+    return gasto;
 }
+
+actualizarPresupuesto(10000);
+console.log(presupuesto);
+console.log(mostrarPresupuesto());
+let gastos = CrearGasto();
+console.log(gastos.descripcion);
+console.log(gastos.valor);
+console.log(gastos.mostrarGasto(gastos.descripcion, gastos.valor));
+console.log(gastos.actualizarDescripcion(gastos.descripcion, "Buenas tarde"));
+console.log(gastos.actualizarValor(gastos.valor, 200))
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
