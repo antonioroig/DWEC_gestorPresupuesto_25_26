c2f82639d92a499e1c7f088ba0cafe1585578815
Juan León <depechejuan@gmail.com>
2025-09-19 15:16:53 +0200

    Definicion de clase y avance ejercicios P1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(value) {
+    if (value < 0) {
+        console.log(`Error en el valor del presupuesto`);
+        return -1;
+    }
+    return value;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    console.log(`Tu presupuesto actual es de: ${presupuesto}€`)
+}
+
+class Gasto {
+    constructor(descripcion, valor) {
+        this.descripcion = descripcion
+        this.valor = valor
+    }
+    mostrarGasto() {
+        const desc = `Gasto correspondiente "${this.descripcion}" con Valor: ${this.valor} €`
+        return desc;
+    }
+    actualizarDescripcion(value) {
+        this.descripcion = value;
+    }
+    actualizarValor(value) {
+        if (value < 0)
+            return;
+        this.valor = value;
+    }
 }
 
 function CrearGasto() {
-    // TODO
+
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
