6e241903f30100cda8ea5af002cfcca6f4af5408
ChristianMontes89 <chrmonper@alu.edu.gva.es>
2025-09-23 20:50:48 +0200

    gestion de presupuesto si acabar , falta pasar test

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,40 @@
+"use strict";
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valorPresupuesto) {
+    if(valorPresupuesto < 0 ){
+        valorPresupuesto = -1;
+    }else{
+        valorPresupuesto = presupuesto;
+    }
+    return valorPresupuesto;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    console.log(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    if( isNaN(valor) || valor < 0){
+        valor = 0;
+    }
+    this.descripcion = descripcion;
+    this.valor = valor;
+
+    this.mostrarGasto = function(){
+        console.log(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);};
+    this.actualizarDescripcion = function(nuevaDescripcion){
+        this.descripcion = nuevaDescripcion;
+    }
+
+    this.actualizarValor = function(nuevoValor) {
+        if (!isNaN(nuevoValor) && nuevoValor >= 0) {
+            this.valor = nuevoValor;
+            }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
