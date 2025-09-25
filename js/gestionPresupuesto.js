bf125fce682e1aa9eadb1327c31ce92219637ad6
Luis <master.lpg7@gmail.com>
2025-09-25 17:30:40 +0200

    solo tengo un fallo(arreglar)

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,24 +1,48 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(cantidad) {
+    if (typeof cantidad === 'number' && cantidad >= 0 && !isNaN(cantidad)) {
+        presupuesto = cantidad;
+        return presupuesto;
+    }else if(cantidad < 0){
+        console.error("Error: El presupuesto debe ser un número no negativo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    
+    if (typeof valor === 'number' && valor >= 0 && !isNaN(valor)) {
+        this.valor = valor;
+    } else {
+        this.valor = 0;
+    }
+    
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+    
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    };
+    
+    this.actualizarValor = function(nuevoValor) {
+        if (typeof nuevoValor === 'number' && nuevoValor >= 0 && !isNaN(nuevoValor)) {
+            this.valor = nuevoValor;
+        }
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+// Si al obtener el código de una práctica se genere un conflicto, por favor incluye todo el código que aparece aquí debajo
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
