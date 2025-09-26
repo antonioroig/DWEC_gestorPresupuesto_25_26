82de68bf7cbc1d9757ecd4555ab10fb5a53d16c4
ChristianMontes89 <chrmonper@alu.edu.gva.es>
2025-09-26 16:40:36 +0200

    gestion de presupuesto acabado

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,42 +1,48 @@
 "use strict";
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+// Variable global
 let presupuesto = 0;
 
+// Función para actualizar el presupuesto
 function actualizarPresupuesto(valorPresupuesto) {
-    if(valorPresupuesto < 0 ){
-        valorPresupuesto = -1;
-    }else{
-        valorPresupuesto = presupuesto;
+    if (valorPresupuesto >= 0 && !isNaN(valorPresupuesto)) {
+        presupuesto = valorPresupuesto;
+        return presupuesto;
+    } else {
+        return -1;
     }
-    return valorPresupuesto;
 }
 
+// Función para mostrar el presupuesto
 function mostrarPresupuesto() {
-    console.log(`Tu presupuesto actual es de ${presupuesto} €`);
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
+// Constructor para crear gastos
 function CrearGasto(descripcion, valor) {
-    if( isNaN(valor) || valor < 0){
-        valor = 0;
-    }
     this.descripcion = descripcion;
-    this.valor = valor;
 
-    this.mostrarGasto = function(){
-        console.log(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);};
-    this.actualizarDescripcion = function(nuevaDescripcion){
+    if (valor >= 0 && !isNaN(valor)) {
+        this.valor = valor;
+    } else {
+        this.valor = 0;
+    }
+
+    this.mostrarGasto = function () {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+
+    this.actualizarDescripcion = function (nuevaDescripcion) {
         this.descripcion = nuevaDescripcion;
-    }
+    };
 
-    this.actualizarValor = function(nuevoValor) {
-        if (!isNaN(nuevoValor) && nuevoValor >= 0) {
+    this.actualizarValor = function (nuevoValor) {
+        if (nuevoValor >= 0 && !isNaN(nuevoValor)) {
             this.valor = nuevoValor;
-            }
-    }
-}
+        }
+    };
 
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
