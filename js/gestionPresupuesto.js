78fa64e479f64b71c5c7b4c050b7100f001ad4a9
Javier <javninpen@alu.edu.gva.es>
2025-09-29 18:49:07 +0200

    Añadir funciones para gestionar gastos: listar, añadir, borrar, calcular total y balance

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -41,7 +41,21 @@ function CrearGasto(descripcion, valor) {
         }
     }
 }
+function listarGastos(){
+    return gastos
+}
+function anyadirGasto(){
 
+}
+function borrarGasto(){
+    
+}
+function calcularTotalGastos(){
+    
+}
+function calcularBalance(){
+    
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -49,5 +63,10 @@ function CrearGasto(descripcion, valor) {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto, 
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
