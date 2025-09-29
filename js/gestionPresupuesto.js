ec0248bee1e88a56e64b0e0d86cc6014f696ff72
Jordi Varo <jordivarofernandez@gmail.com>
2025-09-29 20:45:25 +0200

    anyadirGasto hecho

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -78,8 +78,10 @@ function CrearGasto(descripcion, valor = 0, fecha, ...etiquetas) {
 function listarGastos(){
     return gastos;
 }
-function anyadirGasto(){
-
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
 }
 function borrarGasto(){
 
