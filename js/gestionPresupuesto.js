b3d3c76a3c42b40734151d9cfb1ad5402d954de0
alegimlop <alegimlop@alu.edu.gva.es>
2025-09-29 19:26:27 +0200

    asignación de la variable fecha dentro de la función CreaciónGasto dependiendo del contenido de la variable

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -27,7 +27,7 @@ function CrearGasto(descripcion, valor, fecha, etiqueta) {
     this.descripcion = descripcion;
     this.valor = (!isNaN(valor) && valor >= 0) ? valor : 0;
     this.etiqueta = (etiqueta !== undefined) ? etiqueta : [];
-    
+    this.fecha = (fecha !== undefined) ? fecha : getDate();
     this.mostrarGasto = function(){
         return `Gasto correspondiente a ${descripcion} con valor ${valor} €`;
     }
