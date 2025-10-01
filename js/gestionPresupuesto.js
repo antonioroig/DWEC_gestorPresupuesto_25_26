c5b8fdc59196fe12c9160f5c8a73080b5a886f22
CristianGomezLopez99 <crigomlop3@alu.edu.gva.es>
2025-10-01 17:11:06 +0200

    creacion metodo actualizarValor que sea un número positivo y si no , lo deja igual

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -24,13 +24,16 @@ function CrearGasto(descripcion,valor) {
     
     this.descripcion = descripcion
     this.valor = (!isNaN(valor) && valor >= 0) ? valor : 0;
-    this.mostraGasto = function(){
+    this.mostrarGasto = function(){
         return "Gasto correspondiente a " + descripcion + " con valor " + valor + " €"
     }
     this.actualizarDescripcion = function(descripcion){
          this.descripcion = descripcion
     }
-    
+    this.actualizarValor = function(valor){
+        this.valor = (!isNaN(valor) && valor >= 0) ? valor : this.valor;
+       
+            }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
