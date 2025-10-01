7459ae78c1d449ebfd5d3ac4e5da51c8e86fe3e5
PAULA ESCRIBANO REINA <pauescrei@alu.edu.gva.es>
2025-10-01 16:24:18 +0200

    Paula Escribano Reina

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,24 +1,46 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if (typeof valor !== "number" || valor <= 0) {
+    return -1;
+  }
+  presupuesto = valor;
+  return presupuesto;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+  this.descripcion = descripcion;
+  this.valor = typeof valor === "number" && valor >= 0 ? valor : 0;
+
+  this.mostrarGasto = function () {
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+  };
+
+  this.actualizarDescripcion = function (nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion;
+    return this.descripcion;
+  };
+
+  this.actualizarValor = function (nuevoValor) {
+    if (typeof nuevoValor === "number" && nuevoValor >= 0) {
+      this.valor = nuevoValor;
+    }
+    return this.valor; 
+  };
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
