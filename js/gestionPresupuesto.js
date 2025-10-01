1063b833bab8bf05b2657999f998f0e1ad1a04a7
Tonizhan325 <angelzhan666@gmail.com>
2025-10-01 18:34:35 +0200

    Intro Javascript II acabada y testeada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -16,7 +16,8 @@ function actualizarPresupuesto(valor) {
 function mostrarPresupuesto() {
     return "Tu presupuesto actual es de " + presupuesto + " €";
 }
-
+    
+/*
 function CrearGasto(des, value) {
 
     this.descripcion = des;
@@ -38,63 +39,74 @@ function CrearGasto(des, value) {
         this.descripcion = description;
     }
 }
-
+*/
 
 
 
 
 //FUNDAMENTOS JAVASCRIPT II
-/*
+
 let gastos = [];
 let idGasto = 0;
 
-function CrearGasto(description, value, date, ...etiquets) {
+function CrearGasto(description, value, date, ...tags) {
+    this.descripcion = description;
+    if (value > 0)
+        this.valor = value;
+    else
+        this.valor = 0;
+    if (date == undefined || Date.parse(date) == NaN)
+        this.fecha = new Date();
+    else
+        this.fecha = Date.parse(date);
 
-    let gasto = {
-        descripcion: description,
-        valor: value,
-        fecha: Date.parse(date),
-        etiquetas: etiquets,
-        mostrarGastoCompleto: function() {
-            console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + ` €
-Fecha: ` + this.fecha.toLocaleString() + `
-Etiquetas:`) 
-            for (let i = 0; i < this.etiquetas.length; i++)
-                console.log("- " + this.etiquetas[i]);
-        },
-        actualizarDescripcion: function(descripcionNueva) {
-            this.descripcion = descripcionNueva;
-        },
-        actualizarValor: function(valorNuevo){
-            if (valorNuevo < 0)
-                return;
-            this.valor = valorNuevo;
-        },
-        actualizarFecha: function(date){
-            if (Date.parse(date) == undefined)
-                return;
-            this.fecha = date;
-        },
-        anyadirEtiquetas: function(...etiquetas){
-            for(let i = 0; i < etiquetas.length; i++)
-                if (this.etiquetas.includes(etiquetas[i]) == false)
-                    this.etiquetas.push(etiquetas[i]);
-        },
-        borrarEtiquetas: function(...etiquetas){
-            for(let i = 0; i < etiquetas.length; i++){
-                if (this.etiquetas.includes(etiquetas[i])){
-                    let index = this.etiquetas.indexOf(etiquetas[i]);
-                    this.etiquetas.splice(index, 1);
-                }
+    if (tags == undefined || tags.length === 0)
+        this.etiquetas = [];
+    else
+        this.etiquetas = tags;
+
+    this.mostrarGastoCompleto = function(){
+        let allTags = "";
+        let fechaFormateada = new Date(this.fecha)
+
+        for (let i = 0; i < this.etiquetas.length; i++)
+            allTags = allTags + "- " + this.etiquetas[i] + "\n";
+        
+        return `Gasto correspondiente a ` + this.descripcion +  ` con valor `  + this.valor +  ` €.\nFecha: ` + fechaFormateada.toLocaleString() +`\nEtiquetas:\n` + allTags;
+    }
+
+    this.actualizarValor = function(value){
+        if (value > 0)
+            this.valor = value;
+    }
+
+    this.actualizarDescripcion = function(description){
+        this.descripcion = description;
+    }
+
+    this.actualizarFecha = function(date){
+        if (!isNaN(Date.parse(date)))
+            this.fecha = Date.parse(date);
+    }
+    
+    this.anyadirEtiquetas = function(...tags){
+        for (let i = 0; i < tags.length; i++)
+        {
+            if(this.etiquetas.includes(tags[i]) == false)
+                this.etiquetas.push(tags[i]);
+        }
+    }
+
+    this.borrarEtiquetas = function(...tags){
+        for (let i = tags.length - 1; i >= 0; i--)
+        {
+            if(this.etiquetas.includes(tags[i]))
+            {
+                let index = this.etiquetas.indexOf(tags[i])
+                this.etiquetas.splice(index, 1);
             }
         }
     }
-    if (value < 0 || value == undefined)
-        gasto.valor = 0;
-    if (etiquets.length == 0)
-        gasto.etiquetas = [];
-    
-    return gasto;
 }
 
 function listarGastos()
@@ -104,25 +116,28 @@ function listarGastos()
     return gastos;
 }
 
-function anyadirGasto(...gasto)
+function anyadirGasto(gasto)
 {
     gasto.id = idGasto;
     idGasto++;
-    gastos.push(...gasto);
+    gastos.push(gasto);
 }
 
-function borrarGasto(gasto)
+function borrarGasto(id)
 {
-    gastos.splice(gasto.id, 1);
+    for (let i = 0; i < gastos.length; i++)
+    {
+        if (id === gastos[i].id)
+            gastos.splice(i, 1);
+    }
 }
 
 function calcularTotalGastos()
 {
-    let sumaTotal = 0;
-    for(let i = 0; i < gastos.length; i++)
-        if (gastos[i].valor != undefined)
-            sumaTotal += gastos[i].valor;
-    return sumaTotal;
+    let total = 0;
+    for (let i = 0; i < gastos.length; i++)
+        total += gastos[i].valor;
+    return total;
 }
 
 function calcularBalance()
@@ -130,8 +145,6 @@ function calcularBalance()
     let gastosTotales = calcularTotalGastos();
     return presupuesto - gastosTotales;
 }
-
-listarGastos();
 
 let gasto1 = new CrearGasto("Gasto 1");
 let gasto2 = new CrearGasto("Gasto 2", 23.55);
@@ -141,20 +154,16 @@ let gasto5 = new CrearGasto("Gasto 5", 23.55, "2021-10-06T13:10", "casa", "super
 let gasto6 = new CrearGasto("Gasto 6", 23.55, "2021-10-06T13:10", "casa", "supermercado", "comida" );
 
 
-
-aBalance();
-console.log(balance);
-*/
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto/*,
+    CrearGasto,
     listarGastos,
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance*/
+    calcularBalance
 }
