2cc2c6c7e292fe53109b534e122201a8de8f84db
AlejandroGarciaPerez1999 <alegarper10@alu.edu.gva.es>
2025-10-02 15:54:50 +0200

    Funcion anyadirGasto finalizada y probada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -81,8 +81,10 @@ function listarGastos(){
     return gastos;
 }
 
-function anyadirGasto(){
-
+function anyadirGasto(gasto){
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
 }
 
 function borrarGasto(){
