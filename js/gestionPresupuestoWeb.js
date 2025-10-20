15f2d2002ba27bfaa31549677d053d9c89a81832
Juan León <depechejuan@gmail.com>
2025-10-20 20:41:43 +0200

    Creacion de archivos necesarios y estructura basica

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,23 @@
+
+
+function mostrarDatoEnId(id, valor) {
+    let idElement = id;
+}
+
+
+function mostrarGastoWeb(id, gasto) {
+
+}
+
+
+function mostrarGastosAgrupadosWeb() {
+
+}
+
+
+
+module.exports = {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
