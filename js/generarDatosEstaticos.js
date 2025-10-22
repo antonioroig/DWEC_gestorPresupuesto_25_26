c67d6044f2bf3e552abd4939f5ca6df069408fca
ChristianMontes89 <chrmonper@alu.edu.gva.es>
2025-10-22 16:14:42 +0200

    creado funcion mostrarDatoEnId

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,5 +1,11 @@
 "use strict";
+// Importar módulos
+import * as gestionPresupuesto from './gestionPresupuesto.js';
+import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
 
+// Generar datos estáticos para pruebas
+gestionPresupuesto.actualizarPresupuesto(1500);
+// gestionPresupuesto.mostrarPresupuesto();
 export{
 
 }
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,5 +1,15 @@
 "use strict";
 
+//función para mostrar dato en id
+function mostrarDatoEnId(idElemento, valor){
+    let elemento = document.getElementById(idElemento);
+    if(elemento){
+        elemento.textContent = valor;
+    }
+}
+
+
+//función para mostrar gastos agrupados en web
 export{
     mostrarDatoEnId,
     mostrarGastoWeb,
