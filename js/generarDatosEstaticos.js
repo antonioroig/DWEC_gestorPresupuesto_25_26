0660ebb3be596bc1a942dddafb4821ceaec95bf0
Juan León <depechejuan@gmail.com>
2025-10-22 19:59:22 +0200

    Test 1 completo - Funciones mostrarDatoEnId y MostrarGastoWeb

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,30 @@
+import * as gp from './gestionPresupuesto.js'
+import * as gpw from './gestionPresupuestoWeb.js'
+
+// Parte 1 (Test 1)
+gp.actualizarPresupuesto(1500);
+let presupuesto = gp.mostrarPresupuesto();
+console.log(presupuesto);
+gpw.mostrarDatoEnId("presupuesto", presupuesto)
+
+// Parte 2 (Test 1)
+let g1 = new gp.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida")
+let g2 = new gp.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida")
+let g3 = new gp.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte")
+let g4 = new gp.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina")
+let g5 = new gp.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros")
+let g6 = new gp.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
+
+gp.anyadirGasto(g1)
+gp.anyadirGasto(g2)
+gp.anyadirGasto(g3)
+gp.anyadirGasto(g4)
+gp.anyadirGasto(g5)
+gp.anyadirGasto(g6)
+
+let total = gp.calcularTotalGastos();
+gpw.mostrarDatoEnId("gastos-totales", total)
+let balance = gp.calcularBalance()
+gpw.mostrarDatoEnId("balance-total", balance)
+
+// Parte 3 (Test 2)
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,11 +1,40 @@
-
-
 function mostrarDatoEnId(id, valor) {
-    let idElement = id;
+    let tag = document.getElementById(id);
+    tag.textContent = valor;
 }
 
-
 function mostrarGastoWeb(id, gasto) {
+    let idElement = document.getElementById(id);
+    
+
+    let body = document.body;
+    let mainDiv = document.createElement("div");
+    mainDiv.setAttribute("gasto")
+    body.append(mainDiv);
+
+    let desc = mainDiv.createElement("div")
+    desc.setAttribute("gasto-descripcion")
+    desc.textContent = gasto.descripcion;
+    mainDiv.append(desc)
+
+    let fecha = mainDiv.createElement("div")
+    fecha.setAttribute("gasto-fecha")
+    fecha.textContent = gasto.fecha;
+    mainDiv.append(fecha)
+
+    let valor = mainDiv.createElement("div")
+    valor.setAttribute("gasto-valor")
+    valor.textContent = gasto.valor;
+
+    let etiqueta = mainDiv.createElement("div");
+    etiqueta.setAttribute("gasto-etiquetas")
+    mainDiv.append(etiqueta);
+    for (let tag in gasto.etiqueta) {
+        let element = body.createElement("span")
+        element.setAttribute("gasto-etiquetas-etiqueta")
+        element.textContent = tag;
+        etiqueta.append(element);
+    }
 
 }
 
@@ -16,7 +45,7 @@ function mostrarGastosAgrupadosWeb() {
 
 
 
-module.exports = {
+export {
     mostrarDatoEnId,
     mostrarGastoWeb,
     mostrarGastosAgrupadosWeb
