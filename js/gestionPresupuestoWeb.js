aff564c1ead68e2a558fb0008ccb2832eeac73a0
CristianGomezLopez99 <crigomlop3@alu.edu.gva.es>
2025-10-22 16:50:26 +0200

    estructura

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,17 @@
+function mostrarDatoEnId(valor,idElemento){
+
+}
+
+function mostrarGastoWeb(){
+
+}
+
+function mostrarGastosAgrupadosWeb(){
+
+}
+
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
