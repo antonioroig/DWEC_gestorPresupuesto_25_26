232b3a830e214479a06acfcc02b210f47ffdecbf
JordiVF <jordivarofernandez@gmail.com>
2025-10-27 00:25:32 +0100

    funcion mostrarGastoWeb hecha sin pasar el test

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -6,7 +6,45 @@ function mostrarDatoEnId(idElemento,valor){
     }
 }
 function mostrarGastoWeb(idElemento, gasto){
+let contenedor = document.getElementById(idElemento);
 
+if (!contenedor){
+    return;
+}
+
+let divGasto = document.createElement("div");
+divGasto.className = "gasto";
+
+let divDescripcion = document.createElement("div");
+divDescripcion.className = "gasto-descripcion";
+divDescripcion.textContent = gasto.descripcion;
+divGasto.appendChild(divDescripcion);
+
+let divFecha = document.createElement("div");
+divFecha.className = "gasto-fecha";
+let fecha = new Date(gasto.fecha);
+divFecha.textContent = fecha.toLocaleDateString();
+divGasto.appendChild(divFecha);
+
+let divValor = document.createElement("div");
+divValor.className = "gasto-valor";
+divValor.textContent = gasto.valor;
+divGasto.appendChild(divValor);
+
+let divEtiquetas = document.createElement("div");
+divEtiquetas.className = "gasto-etiquetas";
+
+if (gasto.etiquetas && gasto.etiquetas.length > 0){
+    gasto.etiquetas.forEach(element => {
+        let span = document.createElement("span");
+        span.className = "gasto-etiquetas-etiqueta";
+        span.textContent = element;
+        divEtiquetas.appendChild(span);
+    });
+}
+
+divGasto.appendChild(divEtiquetas);
+contenedor.appendChild(divGasto);
 }
 function mostrarGastosAgrupadosWeb(){
 
