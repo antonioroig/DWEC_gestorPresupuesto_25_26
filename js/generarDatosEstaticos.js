f392627cde6982ff293675a2223275ebf0d196d9
alegimlop <alegimlop@alu.edu.gva.es>
2025-10-29 16:57:34 +0100

    mostrar presupuesto usando las funciones mostrarPresupuesto y mostrarDatoEnId desde generarDatosEstaticos.js

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,3 +2,7 @@ import * as gp from './gestionPresupuesto.js'
 import * as gW from './gestionPresupuestoWeb.js'
 
 gp.actualizarPresupuesto(1500);
+
+const preActual = gp.mostrarPresupuesto();
+gW.mostrarDatoEnId(presupuesto, preActual);
+
