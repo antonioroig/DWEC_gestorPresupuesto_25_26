3ce38ee290e97d555f1e346702b868a1264ee6e4
alegimlop <alegimlop@alu.edu.gva.es>
2025-10-29 16:58:26 +0100

    creacion de los gastos y añdarlos mediante las funciones crearGasto y anyadirGasto desde generarDatosEstaticos.js

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -6,3 +6,16 @@ gp.actualizarPresupuesto(1500);
 const preActual = gp.mostrarPresupuesto();
 gW.mostrarDatoEnId(presupuesto, preActual);
 
+const gasto1 = gp.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+const gasto2 = gp.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+const gasto3 = gp.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+const gasto4 = gp.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+const gasto5 = gp.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+const gasto6 = gp.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gp.anyadirGasto(gasto1);
+gp.anyadirGasto(gasto2);
+gp.anyadirGasto(gasto3);
+gp.anyadirGasto(gasto4);
+gp.anyadirGasto(gasto5);
+gp.anyadirGasto(gasto6);
