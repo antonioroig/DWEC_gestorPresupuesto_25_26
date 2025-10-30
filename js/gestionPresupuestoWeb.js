37f29c82233aa2171d5d5749d816c507e36e1352
AlejandroGarciaPerez1999 <alegarper10@alu.edu.gva.es>
2025-10-30 17:32:43 +0100

    Funcion mostrarGastosWeb terminada y gastos

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -9,8 +9,36 @@ function mostrarDatoEnId(idElemento, valor){
     }
 }
 
-function mostrarGastoWeb(){
+function mostrarGastoWeb(idElemento , gasto){
+    let elemento = document.getElementById(idElemento);
+    if (elemento) {
+        let divGasto = document.createElement("div");
+        divGasto.classList.add("gasto");
+    }
+    let divDescripcion = document.createElement("div");
+    divDescripcion.classList.add("gasto-descripcion");
+    divDescripcion.textContent = "DESCRIPCIÓN DEL GASTO";
+    divGasto.appendChild(divDescripcion);
 
+    let divFecha = document.createElement("div");
+    divFecha.classList.add("gasto-fecha");
+    // let fechaLocal = new Date(gasto.fecha).toLocaleString();
+    divFecha.textContent = "FECHA DEL GASTO";
+    divGasto.appendChild(divFecha);
+
+    let divValor = document.createElement("div");
+    divValor.classList.add("gasto-valor");
+    divValor.textContent = "VALOR DEL GASTO";
+    divGasto.appendChild(divValor);
+
+    let divEtiquetas = document.createElement("div");
+    divEtiquetas.classList.add("gasto-etiquetas");
+    divGasto.appendChild(divEtiquetas);
+    
+
+
+
+    
 }
 
 function mostrarGastosAgrupadosWeb(){
