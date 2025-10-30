6600596c34569c73c0f66e5edfc001a1224a0882
alegimlop <alegimlop@alu.edu.gva.es>
2025-10-30 13:33:54 +0100

    creación de la función mostrarGastoWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -4,14 +4,14 @@ import * as gW from './gestionPresupuestoWeb.js'
 gp.actualizarPresupuesto(1500);
 
 const preActual = gp.mostrarPresupuesto();
-gW.mostrarDatoEnId(presupuesto, preActual);
+gW.mostrarDatoEnId("presupuesto", preActual);
 
-const gasto1 = gp.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
-const gasto2 = gp.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
-const gasto3 = gp.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
-const gasto4 = gp.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
-const gasto5 = gp.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
-const gasto6 = gp.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+const gasto1 = gp.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+const gasto2 = gp.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+const gasto3 = gp.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+const gasto4 = gp.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+const gasto5 = gp.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+const gasto6 = gp.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
 
 gp.anyadirGasto(gasto1);
 gp.anyadirGasto(gasto2);
@@ -21,7 +21,7 @@ gp.anyadirGasto(gasto5);
 gp.anyadirGasto(gasto6);
 
 const calcTotGasto = gp.calcularTotalGastos();
-gW.mostrarDatoEnId(gastos-totales, calcTotGasto);
+gW.mostrarDatoEnId("gastos-totales", calcTotGasto);
 
 const balance = gp.calcularBalance();
-gW.mostrarDatoEnId(balance-total, balance);
+gW.mostrarDatoEnId("balance-total", balance);
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -5,10 +5,26 @@ function mostrarDatoEnId(idElemento, valor){
 function mostrarGastoWeb(idElemento, gasto){
     const elem = document.getElementById(idElemento)
     if(elem){
-        
+        let etiquetasHTML = "";
+        if(Array.isArray(gasto.etiquetas)){
+            for(let i = 0; i < gasto.etiquetas.lenght; i++){
+                etiquetasHTML += `<span class="gasto-etiquetas-etiqueta">${gasto.etiquetas[i]}</span>`;
+            }
+        } 
+     const gastoHTML = `
+    <div class="gasto">
+      <div class="gasto-descripcion">${gasto.descripcion}</div>
+      <div class="gasto-fecha">${gasto.fecha}</div>
+      <div class="gasto-valor">${gasto.valor}</div>
+      <div class="gasto-etiquetas">
+        ${etiquetasHTML}
+      </div>
+    </div>
+  `;
+        elem.innerHTML += gastoHTML;
     }
     else{
-        alert("El elemento seleccionado no existe");
+        alert(`El elemento ${idElemento} no existe`);
     }
 }
 
