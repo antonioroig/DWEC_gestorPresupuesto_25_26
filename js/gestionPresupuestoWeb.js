8a645046c8ee63944992f5fd575b8974257ca761
Javier <javninpen@alu.edu.gva.es>
2025-10-31 19:15:33 +0100

    Añadir funciones para mostrar datos y gastos en la interfaz web

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,14 @@
+function mostrarDatoEnId(idElemento, valor) {
+    document.getElementById(idElemento).innerHTML += valor
+}
+function mostrarGastoWeb(idElemento, listaGastos) {
+
+}
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+
+}
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
