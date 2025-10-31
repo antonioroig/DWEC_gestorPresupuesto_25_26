182ef8415e45136de7d7c96ec88c6fd14b2cbf57
Javier <javninpen@alu.edu.gva.es>
2025-10-31 19:16:12 +0100

    Añadir lógica para mostrar gastos con etiquetas y formatear fechas en la interfaz web

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -2,7 +2,30 @@ function mostrarDatoEnId(idElemento, valor) {
     document.getElementById(idElemento).innerHTML += valor
 }
 function mostrarGastoWeb(idElemento, listaGastos) {
+    listaGastos.forEach(element => {
+        let contenido = "<div class=gasto>"
+        let gastoFiltrado = Object.fromEntries(Object.entries(element).filter(([k, v]) => typeof v !== "function"))
 
+        for (let gasto in gastoFiltrado) {
+            if (gasto === "etiquetas") {
+                contenido += `<div class = gasto-${gasto}>${gasto}:<br>`
+                gastoFiltrado[gasto].forEach(element => {
+                    contenido += `<span class = gasto-etiquetas-etiqueta>${element}</span><br>`
+                });
+                contenido += `</div>`
+            }
+            else if (gasto !== "fecha") {
+                contenido += `<div class = gasto-${gasto}>${gasto}: ${gastoFiltrado[gasto]}</div>`
+            }
+
+            else {
+                contenido += `<div class = gasto-${gasto}>${gasto}: ${(new Date(gastoFiltrado[gasto])).toISOString().slice(0, 10)}</div>`
+            }
+
+        }
+        contenido += "</div>"
+        mostrarDatoEnId(idElemento, contenido)
+    });
 }
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
 
