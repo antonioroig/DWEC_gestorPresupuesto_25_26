b612d12ebf718e6a0545d32d930de0a37c509965
PAULA ESCRIBANO REINA <pauescrei@alu.edu.gva.es>
2025-10-31 18:28:51 +0100

    presupuesto actualizado y mostrandose en el div

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,2 +1,7 @@
 import * as gp from "./gestionPresupuesto.js";
 import * as web from "./gestionPresupuestoWeb.js";
+
+function main() {
+  gp.actualizarPresupuesto(1500);
+  web.mostrarDatoEnId("presupuesto", gp.mostrarPresupuesto());
+}
