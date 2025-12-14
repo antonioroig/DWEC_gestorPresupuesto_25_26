0f0bf08113ff3beac7152a824fe416386e3375e3
JordiVF <jordivarofernandez@gmail.com>
2025-12-14 17:25:25 +0100

    Funcion transformarListadoEtiquetas completa y pasando tests

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -163,7 +163,12 @@ function agruparGastos(periodo = "mes", etiquetas = [], fechaDesde, fechaHasta) 
 
     return agrupados;
 }
+function transformarListadoEtiquetas(etiquetas){
+    let regex = /[,\.\:;\s]+/;
 
+    return etiquetas.split(regex);
+
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -178,5 +183,6 @@ export   {
     calcularTotalGastos,
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
