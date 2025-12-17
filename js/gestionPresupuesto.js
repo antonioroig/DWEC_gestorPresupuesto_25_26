15f9546c92b2a23d159d34079bd6bcb37d4059c3
Rubén Marhuenda <ruben.marhuenda@iesdoctorbalmis.com>
2025-12-17 18:46:12 +0100

    Añado el codigo a la funcion transformarListadoEtiquetas para que funcione el separar la cadena de etiquetas en un array de etiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -158,6 +158,11 @@ function agruparGastos(periodo, etiquetas, fechaDesde, fechaHasta){
     return result;
 }
 
+function transformarListadoEtiquetas(cadena){
+    let arrFinal = cadena.split(/[,.;:\s]+/);
+    return arrFinal;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -171,5 +176,6 @@ export   {
     calcularTotalGastos,
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
